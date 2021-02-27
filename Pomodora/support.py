ef75197cdeeca132e61aee550fc9d24edec716b9
hj3108 <69028741+hj3108@users.noreply.github.com>
2021-02-27 20:44:44 +0530

    Add files via upload

---
+++ Pomodora/support.py
@@ -0,0 +1,7 @@
+from cx_Freeze import setup, Executable
+setup(
+    name="Pomodoro",
+    version="0.1",
+    description="You can manage your time with this program.",
+    executables= [Executable(r"E:\Python Tutorials\programs\app brewery\Pomodora\pomodoro.py")]
+) 
